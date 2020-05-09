fee12dbdc41b1a8b7ba050736d4489c284547262
Syed Murtza <syed.murtza@gmail.com>
2020-05-09 07:21:05 +0500

    many updates

--- src/SeekerLogin.js
+++ src/SeekerLogin.js
@@ -1,52 +1,45 @@
 import React, {useState} from 'react'
-import {View, Text, Link, Button, StyleSheet, SafeAreaView, Image, TouchableOpacity} from 'react-native'
+import {View, Text, StyleSheet, Button, SafeAreaView, Image, TouchableOpacity, BackHandler} from 'react-native'
 import { LinearGradient } from 'expo-linear-gradient';
-import { Container, Header, Content, Icon, Picker, Form } from "native-base";
+import { Picker } from "native-base";
 import {countries} from './utils/consts.js'
 
 function SeekerLogin({ navigation }){
   const [phCode, setPhCode] = useState('+1')
-  return(
-    <Container style={styles.container}>
-      <LinearGradient
-          colors={['#4E35AE', '#775ED7']}
-          style={{
-            position: 'absolute',
-            left: 0,
-            right: 0,
-            top: 0,
-            bottom: 0,
-            // height: '100%',
-          }}
-        />
 
-      <SafeAreaView>
-        <TouchableOpacity
-          onPress={() => navigation.goBack()}
-        >
-          <Image 
-            source={require('../assets/ic_back_w.png')} 
-            style={{
-              width: 40, 
-              height: 30, 
-              marginTop: 20,
-              // position: 'absolute', 
-              // left: 0,
-              // top: 20
-              }} />
-        </TouchableOpacity>
-      </SafeAreaView>
+  function handleLogin(){
+    console.log('+++++++++++++++++')
+  }
+  return(
+    <LinearGradient style={styles.container} colors={['#4E35AE', '#775ED7']}>
+      <SafeAreaView style={{flex: 1, width: '90%'}}>
+        <View style={{flex: 2, alignItems: 'flex-start'}}>
+          <TouchableOpacity
+          style={{height: 30, width: 40}}
+            onPress={() => navigation.goBack()}
+          >
+            <Image 
+              source={require('../assets/ic_back_w.png')} 
+              style={{
+                width: 40, 
+                height: 30, 
+                marginTop: 20,
+                // position: 'absolute', 
+                // left: 0,
+                // top: 20
+                }} />
+          </TouchableOpacity>
+        </View>
       
-      <Content>
-        <View style={{}}>
-          <Image source={require('../assets/logo_white_2.png')} style={{width: 150, height: 170, marginTop: 30}} />
+        <View style={{flex: 6, alignItems: 'center'}}>
+          <Image source={require('../assets/logo_white_2.png')} style={{width: 150, height: 170, marginTop: 0}} />
         </View>
         
-        <View style={{}}>
+        <View style={{flex: 1, alignItems: 'center'}}>
           <Text style={{color: '#fff', fontSize: 18}}>Log in as a job seeker</Text>
         </View>
 
-        <Form>
+        <View style={{flex: 3, alignItems: 'center'}}>
           <Picker
             mode="dropdown"
             iosHeader="Country"
@@ -61,26 +54,35 @@ function SeekerLogin({ navigation }){
               )
             })}
           </Picker>
-        </Form>
+        </View>
 
-        <View>
+        <View style={{flex: 2, alignItems: 'center'}}>
+          <TouchableOpacity 
+          style={{width:'100%', backgroundColor: '#fff', paddingTop: 12, paddingBottom: 12, borderRadius: 10, backgroundColor: '#fff'}}
+          onPress={() => handleLogin()}>
+            <Text style={{textAlign: 'center', fontSize: 18, color: '#4834A6'}}>Login</Text>
+          </TouchableOpacity>
+
+        </View>
+
+        <View style={{flex: 3, alignItems: 'center'}}>
           <Text
               style={{marginLeft: 6, color: '#fff', textDecorationLine: 'underline', fontSize: 16}}
               onPress={() => navigation.navigate('ForgotPassword')}
             >Forgot you password?</Text>
         </View>
 
-        <View style={{flex: 1, flexDirection: 'row', alignItems: 'center'}}>
+        <View style={{flex: 1, flexDirection: 'row', justifyContent: 'center'}}>
           <Text style={{color: '#fff', fontSize: 16}}>Dont have an account yet?</Text>
           <Text
             style={{marginLeft: 6, color: '#fff', textDecorationLine: 'underline', fontSize: 16}}
             onPress={() => navigation.navigate('SeekerSignup')}
-          >Sign up</Text>
-        
+            >Sign up</Text>
         </View>
-      </Content>
-      
-    </Container>
+        
+        <View style={{flex: 3, alignItems: 'center'}}></View>
+      </SafeAreaView>
+    </LinearGradient>
   )
 }
 
@@ -89,9 +91,10 @@ export default SeekerLogin;
 const styles = StyleSheet.create({
   container: {
     flex: 1,
-    flexDirection: 'column',
-    // alignItems: 'center',
-    padding: 20,
-    backgroundColor: '#775ED7',
+    alignItems: 'center'
+  },
+  header: {
+    flex: 1,
+    alignItems: 'flex-start'
   },
 });
