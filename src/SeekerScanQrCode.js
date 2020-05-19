608ac8c3ecbb8ebad037f49dd1b109189a20427e
Syed Murtza <syed.murtza@gmail.com>
2020-05-19 23:53:01 +0500

    many updates

--- src/SeekerScanQrCode.js
+++ src/SeekerScanQrCode.js
@@ -1,22 +1,46 @@
-import React from 'react'
-import {
-  SafeAreaView, 
-  View, 
-  Text,
-  TouchableOpacity,
-} from 'react-native'
+import React, { useState, useEffect } from 'react';
+import { Text, View, StyleSheet, Button } from 'react-native';
+import { BarCodeScanner } from 'expo-barcode-scanner';
 
 function SeekerScanQrCode({navigation}){
-  return(
-    <SafeAreaView>
-      <View>
-        <Text>SeekerScanQrCode</Text>
-        <TouchableOpacity onPress={() => navigation.navigate('HomeScreen')}>
-          <Text>Home</Text>
-        </TouchableOpacity>
-      </View>
-    </SafeAreaView>
-  )
+  const [hasPermission, setHasPermission] = useState(null);
+  const [scanned, setScanned] = useState(false);
+  
+  useEffect(() => {
+    (async () => {
+      const { status } = await BarCodeScanner.requestPermissionsAsync();
+      setHasPermission(status === 'granted');
+    })();
+  }, []);
+
+  const handleBarCodeScanned = ({ type, data }) => {
+    setScanned(true);
+    alert(`Bar code with type ${type} and data ${data} has been scanned!`);
+
+  };
+
+  if (hasPermission === null) {
+    return <Text>Requesting for camera permission</Text>;
+  }
+  if (hasPermission === false) {
+    return <Text>No access to camera</Text>;
+  }
+
+  return (
+    <View
+      style={{
+        flex: 1,
+        flexDirection: 'column',
+        justifyContent: 'flex-end',
+      }}>
+      <BarCodeScanner
+        onBarCodeScanned={scanned ? undefined : handleBarCodeScanned}
+        style={StyleSheet.absoluteFillObject}
+      />
+
+      {scanned && <Button title={'Tap to Scan Again'} onPress={() => setScanned(false)} />}
+    </View>
+  );
 }
 
 export default SeekerScanQrCode;
