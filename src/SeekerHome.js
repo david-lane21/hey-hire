27e9641b95289cac2bfbad29c68519f258ad44b0
Syed Murtza <syed.murtza@gmail.com>
2020-05-29 21:38:09 +0500

    many updates

--- src/SeekerHome.js
+++ src/SeekerHome.js
@@ -44,6 +44,11 @@ function SeekerHome({navigation}){
     // return () => setProfile({})
   }, [])
   
+  function dateFormat(date){
+    // console.log(date)
+    let d = new Date(date)
+    return `${d.getMonth()}.${d.getDate()}.${d.getFullYear()}`
+  }
   
   return(
     <LinearGradient 
@@ -84,35 +89,45 @@ function SeekerHome({navigation}){
           <Text style={{color: '#fff', fontSize: 22}}>{user.first_name} {user.last_name}</Text>
         </View>
 
-        <View style={{flex: 1, alignItems: 'center', }}>
-          <Text style={{color: '#fff'}}>{user.education}</Text>
+        <View style={{flex: 1, flexDirection:'row', alignItems: 'center', alignSelf: 'center', marginTop: 5}}>
+          <Image source={require('../assets/ic_graduation.png')} style={{width: 15, height: 15}} />
+          <Text style={{color: '#fff', marginLeft: 5}}>{user.education}</Text>
         </View>
 
-        <View style={{flex: 1, alignItems: 'center', paddingBottom: 40, borderBottomWidth: 1, 
-          borderBottomColor: '#715FCB', }}>
-          <Text style={{color: '#fff'}}>{user.city}, {user.state}, {user.country}</Text>
+        <View style={{flex: 1, alignItems: 'center', paddingBottom: 40, borderBottomWidth: 1, borderBottomColor: '#715FCB', marginTop: 5}}>
+            <View style={{flex: 1, flexDirection:'row', alignItems: 'center', alignSelf: 'center'}}>
+              <Image source={require('../assets/ic_location.png')} style={{width: 15, height: 15}} />
+              <Text style={{color: '#fff', marginLeft: 5}}>{user.city}, {user.state}, {user.country}</Text>
+            </View>
         </View>
 
         <View style={{flex: 1, alignItems: 'flex-start', borderBottomWidth: 1, 
           borderBottomColor: '#715FCB', }}>
-          <Text style={{color: '#fff', fontSize: 18, paddingLeft: 15, paddingTop: 10}}>Bio</Text> 
-          <Text style={{color: '#fff', padding: 30, }}>{user.bio}</Text>
+          <View style={{flex: 1, flexDirection: 'row', alignItems: 'center', alignContent: 'center', marginTop: 10}}>
+            <Image source={require('../assets/ic_star_white.png')} style={{width: 14, height: 12, marginLeft: 8,}} />
+            <Text style={{color: '#fff', fontSize: 18, marginLeft: 8}}>Bio</Text> 
+          </View>
+          <Text style={{color: '#fff', fontSize: 13, paddingBottom: 30, paddingLeft: 30, paddingTop: 10, paddingRight: 10 }}>{user.bio}</Text>
         </View>
 
         <View style={{flex: 1, alignItems: 'flex-start', borderBottomWidth: 1, 
           borderBottomColor: '#715FCB', paddingBottom: 10}}>
-          <Text style={{color: '#fff', fontSize: 18, paddingLeft: 15, paddingTop: 10, paddingBottom: 20}}>Past positions</Text> 
+          <View style={{flex: 1, flexDirection: 'row', alignItems: 'center', alignContent: 'center'}}>
+            <Image source={require('../assets/ic_past_positions_white.png')} style={{width: 14, height: 12, marginLeft: 8,}} />
+            <Text style={{color: '#fff', fontSize: 18, marginLeft: 8}}>Past positions</Text> 
+          </View>
+          
           {profile.position ? profile.position.map((position) => {
             return(
-              <View style={{flex: 1, paddingLeft: 30, paddingTop: 5, paddingBottom: 5}} key={position.post_id}>
+              <View style={{flex: 1, paddingLeft: 30, paddingTop: 15, paddingBottom: 5}} key={position.post_id}>
                 <View style={{flex: 1, flexDirection: 'row', alignItems: 'center'}}>
-                  <Text style={{color: '#fff', fontSize: 14, paddingBottom: 3}}>{position.from_date} - </Text>
-                  <Text style={{color: '#fff', fontSize: 14, paddingBottom: 3}}>{position.to_date}</Text>
+                  <Text style={{color: '#fff', fontSize: 13, paddingBottom: 3}}>{dateFormat(position.from_date)} - </Text>
+                  <Text style={{color: '#fff', fontSize: 13, paddingBottom: 3}}>{dateFormat(position.to_date)}</Text>
                 </View>
                 <View style={{flex: 1, flexDirection: 'row', alignItems: 'center', width: '80%', flexWrap: 'wrap'}}>
-                  <Text style={{color: '#fff', fontSize: 14, paddingBottom: 3}}>{position.category} - </Text>
-                  <Text style={{color: '#fff', fontSize: 14, paddingBottom: 3}}>{position.company_name} - </Text>
-                  <Text style={{color: '#fff', fontSize: 14, paddingBottom: 3}}>{position.city_name}</Text>
+                  <Text style={{color: '#fff', fontSize: 13, paddingBottom: 3}}>{position.category} - </Text>
+                  <Text style={{color: '#fff', fontSize: 13, paddingBottom: 3}}>{position.company_name} - </Text>
+                  <Text style={{color: '#fff', fontSize: 13, paddingBottom: 3}}>{position.city_name}</Text>
                 </View>
                 <View style={{borderBottomColor: '#715FCB', borderBottomWidth: 1}}></View>
               </View>
