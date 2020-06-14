18c135cf2d33952098507f309632834e2543edf4
Syed Murtza <syed.murtza@gmail.com>
2020-06-14 14:29:23 -0700

    many updates

--- src/SeekerNotifications.js
+++ src/SeekerNotifications.js
@@ -1,21 +1,30 @@
-import React from 'react'
+import React, {useState, useEffect} from 'react'
 import {
   SafeAreaView, 
   View, 
   Text,
+  Image,
   TouchableOpacity,
+  ScrollView,
 } from 'react-native'
 
 function SeekerNotifications({navigation}){
   return(
-    <SafeAreaView>
-      <View>
-        <Text>SeekerNotifications</Text>
-        <TouchableOpacity onPress={() => navigation.navigate('HomeScreen')}>
-          <Text>Home</Text>
-        </TouchableOpacity>
-      </View>
-    </SafeAreaView>
+    <ScrollView style={{flex: 1, backgroundColor: '#fff'}}>
+      <SafeAreaView style={{flex: 1}}>
+        <View style={{flex: 1, alignItems: 'center'}}>
+          <Text  style={{color: '#6F5EC0', fontSize: 16, fontWeight: 'bold'}}>Notifications</Text>
+        </View>
+
+        <View style={{flex: 1, marginTop: 30}}>
+          <Text  style={{color: '#999', fontSize: 13, paddingLeft: 20}}>Notifications</Text>
+        </View>
+
+        <View style={{flex: 1, marginTop: 200}}>
+          <Text  style={{color: '#999', fontSize: 13, textAlign: 'center'}}>No notification yet!</Text>
+        </View>
+      </SafeAreaView>
+    </ScrollView>
   )
 }
 
